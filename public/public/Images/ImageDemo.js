fcb9eb501f490bacdba2e6003f6aab7482efa19b
NaveenReadlang <naveenrealang888@gmail.com>
2024-12-06 10:15:02 +0530

    Create Public Folder

---
+++ public/public/Images/ImageDemo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import indianflag from './ImageDemo/indianflag.jpg'
+
+function ImageDemo() {
+  return (
+    <div>
+      <h1>Images Page</h1>
+      <img src={indianflag}/>
+    </div>
+  )
+}
+
+export default ImageDemo
